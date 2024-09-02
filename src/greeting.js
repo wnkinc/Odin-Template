40873093ec62877188337847959f61247c9c0355
wnkinc <140204064+wnkinc@users.noreply.github.com>
2024-09-02 15:39:19 -0700

    Addairbnb-base and prettier dependencies

--- src/greeting.js
+++ src/greeting.js
@@ -1,11 +1,13 @@
 // greeting.js
-export function loadHomePage() {
-    const bodyElement = document.querySelector('body');
+const loadHomePage = () => {
+  const bodyElement = document.querySelector("body");
 
-    const createH1 = document.createElement('h1');
-    createH1.textContent = 'Hello, World!';
-    bodyElement.appendChild(createH1);
+  const createH1 = document.createElement("h1");
+  createH1.textContent = "Hello, World!";
+  bodyElement.appendChild(createH1);
 
-    const greeting = "Hello, Odinite!";
-    console.log(greeting);
-}
+  const greeting = "Hello, Odinite!";
+  console.log(greeting);
+};
+
+export default loadHomePage;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import 'normalize.css'; // Importing normalize.css from node_modules
+import "normalize.css"; // Importing normalize.css from node_modules
 
 import "./styles.css";
-import { loadHomePage } from "./greeting.js";
+import loadHomePage from "./greeting";
 
 loadHomePage();
