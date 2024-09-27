d7025d00a84cfc50788e19833e1598aacfc62ea6
wnkinc <140204064+wnkinc@users.noreply.github.com>
2024-09-27 15:44:56 -0700

    feat: ability to add <img/> in html

--- webpack.common.js
+++ webpack.common.js
@@ -32,6 +32,14 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
     ],
   },
 };
